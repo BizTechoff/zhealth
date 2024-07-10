45ff2194460868da6582295daa3d193a4be1627a
Motidru <motidru@gmail.com>
2024-07-10 11:18:40 +0300

    started

---
+++ src/server/api.ts
@@ -0,0 +1,20 @@
+import { remultExpress } from 'remult/remult-express'
+import { createPostgresConnection } from 'remult/postgres'
+import { User } from '../app/users/user'
+import { SignInController } from '../app/users/SignInController'
+import { UpdatePasswordController } from '../app/users/UpdatePasswordController'
+import { initRequest } from './server-session'
+import { config } from 'dotenv'
+config() //loads the configuration from the .env file
+
+export const entities = [User]
+export const api = remultExpress({
+  controllers: [SignInController, UpdatePasswordController],
+  entities,
+  initRequest,
+  dataProvider: async () => {
+    if (process.env['NODE_ENV'] === 'production')
+      return createPostgresConnection({ configuration: 'heroku' })
+    return undefined
+  },
+})
